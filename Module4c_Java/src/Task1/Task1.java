ab513e4674364ed8b315c57cbb853e66d6e1131f
arunarh <ogaarunarh@gmail.com>
2026-09-01 16:32:26 +0100

    Add Module 4C Java Task 1

--- Module4c_Java/src/Task1/Task1.java
+++ Module4c_Java/src/Task1/Task1.java
@@ -1,4 +1,14 @@
 package Task1;
 
 public class Task1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("My name is Aruna Oluwasegun");
+        System.out.println("I am learning Java");
+        System.out.println("I am learning Software Test Automation");
+        System.out.println("Welcome to Module 4C Java");
+        System.out.println("I will become a great Automation Tester");
+
+    }
 }
