9dbd5113432e3ba7a8e5841cd6d9c4810629c7ad
arunarh <ogaarunarh@gmail.com>
2026-09-02 15:59:00 +0100

    Complete Task 3 Strings and Concatenation

--- Module4c_Java/src/Task3/Task3.java
+++ Module4c_Java/src/Task3/Task3.java
@@ -1,4 +1,14 @@
 package Task3;
 
 public class Task3 {
+    public static void main(String[] args) {
+
+        int age = 41;
+
+        // Method 1: Using + operator
+        System.out.println("My age is " + age);
+
+        // Method 2: Using concat()
+        System.out.println("My age is ".concat(String.valueOf(age)));
+    }
 }
