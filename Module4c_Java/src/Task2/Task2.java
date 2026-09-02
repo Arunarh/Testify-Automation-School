ef3432969b50d19b29e878586461f87b47644229
arunarh <ogaarunarh@gmail.com>
2026-09-02 15:10:12 +0100

    Complete Task 2 Java data types

--- Module4c_Java/src/Task2/Task2.java
+++ Module4c_Java/src/Task2/Task2.java
@@ -1,4 +1,21 @@
 package Task2;
 
 public class Task2 {
+
+    public static void main(String[] args) {
+
+        int age = 25;
+        double height = 1.75;
+        char grade = 'A';
+        boolean isLearningJava = true;
+        String name = "Aruna";
+        float price = 1500.50f;
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(grade);
+        System.out.println(isLearningJava);
+        System.out.println(name);
+        System.out.println(price);
+    }
 }
