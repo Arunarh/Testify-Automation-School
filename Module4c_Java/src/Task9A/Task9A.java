f1410daf117a07029dffa58e12c113645b726496
arunarh <ogaarunarh@gmail.com>
2026-09-04 17:01:27 +0100

    Complete Task 9A Java Loops

---
+++ Module4c_Java/src/Task9A/Task9A.java
@@ -0,0 +1,15 @@
+package Task9A;
+
+public class Task9A {
+    public static void main(String[] args) {
+
+        for (int number = 1; number <= 10; number++) {
+
+            if (number % 2 == 0) {
+                System.out.println(number + " is Even");
+            } else {
+                System.out.println(number + " is Odd");
+            }
+        }
+    }
+}
