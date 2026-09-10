77cbf78aaafef129d15530f06c2883e8d85d7562
arunarh <ogaarunarh@gmail.com>
2026-09-10 19:55:16 +0100

    Add Task 12 - Access Level Modifiers

---
+++ Module4c_Java/src/Task12/packageA/A.java
@@ -0,0 +1,8 @@
+package Task12.packageA;
+
+public class A {
+
+    public void methodA() {
+        System.out.println("This method can be accessed anywhere in the project.");
+    }
+}
---
+++ Module4c_Java/src/Task12/packageB/B.java
@@ -0,0 +1,8 @@
+package Task12.packageB;
+
+public class B {
+
+    private void methodB() {
+        System.out.println("This method can only be accessed within Class B.");
+    }
+}
