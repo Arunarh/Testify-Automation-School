a399f878ec741f4fa39967e7624dd80fccd14047
arunarh <ogaarunarh@gmail.com>
2026-09-10 15:47:53 +0100

    Complete Task 11

---
+++ Module4c_Java/src/Task11/A.java
@@ -0,0 +1,8 @@
+package Task11;
+
+public class A {
+
+    public String getName(String name) {
+        return name;
+    }
+}
---
+++ Module4c_Java/src/Task11/B.java
@@ -0,0 +1,13 @@
+package Task11;
+
+public class B {
+
+    public static void main(String[] args) {
+
+        A person = new A();
+
+        String name = person.getName("Michael");
+
+        System.out.println(name);
+    }
+}
