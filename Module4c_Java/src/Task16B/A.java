19e06904298555e1eab72e057ddaae26a994e61c
arunarh <ogaarunarh@gmail.com>
2026-09-13 11:12:17 +0100

    Complete Task 16B - Method Overriding

--- Module4c_Java/src/Task16B/A.java
+++ Module4c_Java/src/Task16B/A.java
@@ -1,4 +1,12 @@
 package Task16B;
 
 public class A {
+
+    public void login(String username) {
+        System.out.println("Parent class: Logging in user " + username);
+    }
+
+    public void logout(String username) {
+        System.out.println("Parent class: Logging out user " + username);
+    }
 }
--- Module4c_Java/src/Task16B/B.java
+++ Module4c_Java/src/Task16B/B.java
@@ -1,4 +1,21 @@
 package Task16B;
 
-public class B {
+public class B extends A {
+
+    @Override
+    public void login(String username) {
+        System.out.println("Child class: Welcome " + username + ", login successful!");
+    }
+
+    @Override
+    public void logout(String username) {
+        System.out.println("Child class: Goodbye " + username + ", you have logged out successfully!");
+    }
+
+    public static void main(String[] args) {
+        B user = new B();
+
+        user.login("Michael");
+        user.logout("Michael");
+    }
 }
