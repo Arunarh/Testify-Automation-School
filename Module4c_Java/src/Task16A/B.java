0eb9200d76400b9c0565d2d1e8b60b1afe511791
arunarh <ogaarunarh@gmail.com>
2026-09-13 11:11:18 +0100

    Complete Task 16A - Polymorphism (Overloading and Overriding)

--- Module4c_Java/src/Task16A/B.java
+++ Module4c_Java/src/Task16A/B.java
@@ -1,4 +1,15 @@
 package Task16A;
 
 public class B {
+
+    public static void main(String[] args) {
+
+        A test = new A();
+
+        test.runTest("Login Test");
+        test.runTest("Login Test", 1);
+        test.runTest(2);
+        test.runTest("Payment Test", 3, true);
+        test.runTest(4, "Logout Test", true);
+    }
 }
